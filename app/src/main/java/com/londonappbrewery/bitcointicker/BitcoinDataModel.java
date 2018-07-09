65fc949c4f2608ec2b0c4bbc7f2bdda59216563d
Alif <alif.numan29@gmail.com>
2018-07-09 21:15:53 +0800

    Challenge completed.

---
+++ app/src/main/java/com/londonappbrewery/bitcointicker/BitcoinDataModel.java
@@ -0,0 +1,23 @@
+package com.londonappbrewery.bitcointicker;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class BitcoinDataModel {
+    private String mAsk;
+
+    public static BitcoinDataModel fromJSON(JSONObject jsonObject) {
+        try {
+            BitcoinDataModel bitcoinDataModel = new BitcoinDataModel();
+            bitcoinDataModel.mAsk = jsonObject.getString("ask");
+            return bitcoinDataModel;
+        }catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String getAsk() {
+        return mAsk;
+    }
+}
